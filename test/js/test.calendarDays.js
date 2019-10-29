3a3437a4582e965b5bed1c289100982aacbff14a
TamaPS <55314943+TamaPS@users.noreply.github.com>
2019-10-29 19:50:47 +0100

    Add files via upload

--- test/js/test.calendarDays.js
+++ test/js/test.calendarDays.js
@@ -6,19 +6,19 @@ suite("Testing calcDate", function() {
         expect(calcDate(new Date('2019-10-27'),10)).to.equal('2019-11-06');
     });
     test("Test pasado", function() {
-        expect(calcDate(new Date('2019-10-27'),10)).to.equal('2019-11-06');
+        expect(calcDate(new Date('2019-06-30'),2)).to.equal('2019-07-02');
     });
     test("Test pasado", function() {
-        expect(calcDate(new Date('2019-10-27'),10)).to.equal('2019-11-06');
+        expect(calcDate(new Date('2017-07-25'),200)).to.equal('2018-02-10');
     });
     test("Test pasado", function() {
-        expect(calcDate(new Date('2019-10-27'),10)).to.equal('2019-11-06');
+        expect(calcDate(new Date('2019-10-27'),-1)).to.equal('2019-10-26');
     });
     test("Test pasado", function() {
-        expect(calcDate(new Date('2019-10-27'),10)).to.equal('2019-11-06');
+        expect(calcDate(new Date('2012-02-28'),3)).to.equal('2012-03-02');
     });
     test("Test pasado", function() {
-        expect(calcDate(new Date('2019-10-27'),10)).to.equal('2019-11-06');
+        expect(calcDate(new Date('2019-01-29'),1168)).to.equal('2022-04-11');
     });
 });
 
@@ -27,19 +27,19 @@ suite("Testing getDays", function() {
         expect(getDays(new Date('2019-10-10'),new Date('2019-10-28'))).to.equal(18);
     });
     test("Test pasado", function() {
-        expect(getDays(new Date('2019-10-10'),new Date('2019-10-28'))).to.equal(18);
+        expect(getDays(new Date('2019-06-30'),new Date('2019-07-02'))).to.equal(2);
     });
     test("Test pasado", function() {
-        expect(getDays(new Date('2019-10-10'),new Date('2019-10-28'))).to.equal(18);
+        expect(getDays(new Date('2017-07-25'),new Date('2018-02-10'))).to.equal(200);
     });
     test("Test pasado", function() {
-        expect(getDays(new Date('2019-10-10'),new Date('2019-10-28'))).to.equal(18);
+        expect(getDays(new Date('2019-10-27'),new Date('2019-10-26'))).to.equal(-1);
     });
     test("Test pasado", function() {
-        expect(getDays(new Date('2019-10-10'),new Date('2019-10-28'))).to.equal(18);
+        expect(getDays(new Date('2012-02-28'),new Date('2012-03-02'))).to.equal(3);
     });
     test("Test pasado", function() {
-        expect(getDays(new Date('2019-10-10'),new Date('2019-10-28'))).to.equal(18);
+        expect(getDays(new Date('2019-01-29'),new Date('2022-04-11'))).to.equal(1168);
     });
 });
 
--- test/js/test.workingDays.js
+++ test/js/test.workingDays.js
@@ -9,16 +9,16 @@ suite("Testing calcDate", function() {
         expect(calcWorkingDate(new Date('2019-10-31'),-14)).to.equal('2019-10-11');
     });
     test("Test pasado", function() {
-        expect(calcWorkingDate(new Date('2019-10-31'),-14)).to.equal('2019-10-11');
+        expect(calcWorkingDate(new Date('2017-07-25'),200)).to.equal('2018-05-11');
     });
     test("Test pasado", function() {
-        expect(calcWorkingDate(new Date('2019-10-31'),-14)).to.equal('2019-10-11');
+        expect(calcWorkingDate(new Date('2019-10-27'),-1)).to.equal('2019-10-25');
     });
     test("Test pasado", function() {
-        expect(calcWorkingDate(new Date('2019-10-31'),-14)).to.equal('2019-10-11');
+        expect(calcWorkingDate(new Date('2012-02-28'),3)).to.equal('2012-03-02');
     });
     test("Test pasado", function() {
-        expect(calcWorkingDate(new Date('2019-10-31'),-14)).to.equal('2019-10-11');
+        expect(calcWorkingDate(new Date('2019-01-29'),1168)).to.equal('2023-08-28');
     });
 });
 
@@ -30,15 +30,15 @@ suite("Testing getDays", function() {
         expect(getWorkingDays(new Date('2019-10-13'),new Date('2019-09-30'))).to.equal(-10);
     });
     test("Test pasado", function() {
-        expect(getWorkingDays(new Date('2019-10-13'),new Date('2019-09-30'))).to.equal(-10);
+        expect(getWorkingDays(new Date('2017-07-25'),new Date('2018-02-10'))).to.equal(137);
     });
     test("Test pasado", function() {
-        expect(getWorkingDays(new Date('2019-10-13'),new Date('2019-09-30'))).to.equal(-10);
+        expect(getWorkingDays(new Date('2019-10-27'),new Date('2019-10-26'))).to.equal(0);
     });
     test("Test pasado", function() {
-        expect(getWorkingDays(new Date('2019-10-13'),new Date('2019-09-30'))).to.equal(-10);
+        expect(getWorkingDays(new Date('2012-02-28'),new Date('2012-03-02'))).to.equal(3);
     });
     test("Test pasado", function() {
-        expect(getWorkingDays(new Date('2019-10-13'),new Date('2019-09-30'))).to.equal(-10);
+        expect(getWorkingDays(new Date('2019-01-29'),new Date('2022-04-11'))).to.equal(816);
     });
 });
